cf189e9da47430d4e13e2bd0f83f5425505ad522
rohankapoor <Rohan@2808>
2022-08-07 19:33:44 +0530

    inital commit

---
+++ src/test/java/com/springbootresourcedao/SpringBootResourceDao/SpringBootResourceDaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootresourcedao.SpringBootResourceDao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootResourceDaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
